39879ca3ad40acd3dcbfc5ba2c2caff840f87ff2
tomek <lemures64@gmail.com>
2022-08-21 16:54:24 +0200

    test

--- src/main_app.py
+++ src/main_app.py
@@ -10,24 +10,23 @@ from user_tags_repository import UserTagsRepository
 app = FastAPI()
 
 logger = logging.getLogger("uvicorn")
-#tags_repository = UserTagsRepository()
+tags_repository = UserTagsRepository()
 
 @app.post("/user_profiles{cookie}")
 async def get_tags(cookie: str, time_range: Optional[str], limit: Optional[int]):
     time_start = None if not time_range else time_range.split("_")[1]
     time_end = None if not time_range else time_range.split("_")[0]
-    return {}
-    # views = tags_repository.get_user_tags(cookie=cookie, action="VIEW", time_start=time_start, time_end=time_end)
-    # buys = tags_repository.get_user_tags(cookie=cookie, action="BUY", time_start=time_start, time_end=time_end)
-    # logger.info(f"{cookie} {time_range} {limit}")
-    # return {
-    #     "cookie": cookie,
-    #     "views": views[:limit],
-    #     "buys": buys[:limit]
-    # }
+    views = tags_repository.get_user_tags(cookie=cookie, action="VIEW", time_start=time_start, time_end=time_end)
+    buys = tags_repository.get_user_tags(cookie=cookie, action="BUY", time_start=time_start, time_end=time_end)
+    logger.info(f"{cookie} {time_range} {limit}")
+    return {
+        "cookie": cookie,
+        "views": views[:limit],
+        "buys": buys[:limit]
+    }
 
 
 @app.post("/user_tags", status_code=HTTP_204_NO_CONTENT)
 async def add_tag(user_tag: UserTag):
-    #tags_repository.add_user_tag(tag=user_tag)
+    tags_repository.add_user_tag(tag=user_tag)
     logger.info(user_tag.json())

1a789e679a5660b5f5c50256a5cbc3476c9c6c3b
tomek <lemures64@gmail.com>
2022-08-21 17:58:16 +0200

    test

--- src/main_app.py
+++ src/main_app.py
@@ -17,21 +17,6 @@ app = FastAPI()
 logger = logging.getLogger("uvicorn")
 tags_repository = UserTagsRepository()
 
-class RequestLogger(APIRoute):
-    def get_route_handler(self) -> Callable:
-        original_route_handler = super().get_route_handler()
-
-        async def custom_route_handler(request: Request) -> Response:
-            req = await request.body()
-            logger.info(f"Request body\n:{req}")
-            response: Response = await original_route_handler(request)
-            logger.info(f"Response body:\n: {response.body}")
-            return response
-
-        return custom_route_handler
-
-app.router.route_class = RequestLogger
-
 @app.post("/user_profiles/{cookie}")
 async def get_tags(debug_answer: UserProfile, cookie: str, time_range: Optional[str], limit: Optional[int]):
     time_start = None if not time_range else time_range.split("_")[0]
@@ -40,9 +25,9 @@ async def get_tags(debug_answer: UserProfile, cookie: str, time_range: Optional[
     buys = tags_repository.get_user_tags(cookie=cookie, action="BUY", time_start=time_start, time_end=time_end)
     response = UserProfile(cookie=cookie, views=views[:limit], buys=buys[:limit])
     logger.info(f"{cookie} {time_range} {limit}")
-    logger.info(f"Expected answer:\n{debug_answer.json()}")
-    logger.info(f"Sent answer:\n{debug_answer.json()}")
-    return debug_answer
+    # logger.info(f"Expected answer:\n{debug_answer.json()}")
+    # logger.info(f"Sent answer:\n{debug_answer.json()}")
+    return response
 
 
 @app.post("/user_tags", status_code=HTTP_204_NO_CONTENT)
